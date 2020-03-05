59c7fe4cc65349ff5434ef9f968a12c958374979
eugenpolytechnic <zenya.01@mail.ru>
2020-03-05 19:52:40 +0300

    added a two-dimensional List case

--- src/main/kotlin/slices.kt
+++ src/main/kotlin/slices.kt
@@ -1,12 +1,20 @@
 package ru.sbpstu.jblab
 
-operator fun <E> List<E>.get(range: IntProgression): List<E> {
-    return this.slice(range)
+operator fun <E> List<List<E>>.get(range: IntProgression, range2: IntProgression): List<List<E>> {
+    val result = this.slice(range).toMutableList()
+    for (i in result.indices)
+        result[i] = result[i].slice(range2)
+    return result.toList()
 }
+
+operator fun <E> List<E>.get(range: IntProgression): List<E> = this.slice(range)
+
 
 fun main() {
-    val test = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
-    print(test[0..8 step 2])
+    val table = List(3) { List(3) {0} }.toMutableList()
+    table[0] = listOf(1, 2, 3)
+    table[1] = listOf(4, 5, 6)
+    table[2] = listOf(7, 8, 9)
+    print(table.toList()[0..1, 0..1])
 }
-
 
